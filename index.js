b6be1fb05499c3c694b0af88522061c0978d374c
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-09-02 08:04:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
+function GetId(Id) {
+    return document.getElementById(Id)
+}
+    
+var ThisURL = window.location.href
+ThisURL = decodeURIComponent(ThisURL)
+var Query = ThisURL.slice(ThisURL.indexOf("?") + 1)
+if (ThisURL === Query) {
+GetId("Header").innerHTML = "Error Loading Embed. No Query Parameter Specified."
+}
+else {
+  //Valid query. Lets go!
+  Query = Query.trim()
+    
+  //Convert http:// to https://
+  if (Query.indexOf("http://") === 0) {
+        Query = Query.slice(7)
+        Query = "https://" + Query
+  }
+  if (!(Query.indexOf("https://") === 0)) {
+      
+    //If they just don't put the http:// or https:// we can do some things to try and identify what they mean.
+    var check = 0
+    //253 maximum domain length
+    if (Query.length > 253) {
+    check = 1
+    }
 
+    //The ending .(one character) takes 2 characters
+    if (Query.indexOf(".") > 251) {
+    check = 1
+    }
+
+    if (!(/^(?:(?:(?:[a-zA-z\-]+)\:\/{1,3})?(?:[a-zA-Z0-9])(?:[a-zA-Z0-9-\.]){1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+|\[(?:(?:(?:[a-fA-F0-9]){1,4})(?::(?:[a-fA-F0-9]){1,4}){7}|::1|::)\]|(?:(?:[0-9]{1,3})(?:\.[0-9]{1,3}){3}))(?:\:[0-9]{1,5})?$/.test(Query.slice(0, Query.indexOf("/"))))) {
+    check = 1
+    }
+   
+console.log(check)
+      
+    //Check if it passed
+    if (check === 0) {
+    Query = "https://" + Query
+    } 
+      
+  }
+
+    
+  var Embed = document.createElement("iframe")
+  Embed.sandbox = "allow-scripts allow-forms allow-pointer-lock allow-popups"
+  //No same origin
+  if (Query.indexOf("https://") === 0) {
+      Embed.src = Query
+  }
+  else {
+      Embed.srcdoc = Query
+  }
+  Embed.className = "frame"
+  GetId("Container").appendChild(Embed)
+  GetId("Header").innerHTML = ""
+  
+    
+    
+    
+    
+}
